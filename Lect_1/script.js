d0309e404de6ee7128f878ece913e901cd228981
Pavel <pavel@b2profit.ru>
2024-12-18 23:51:03 +0300

    1 лекция

--- Lect_1/script.js
+++ Lect_1/script.js
@@ -2,8 +2,6 @@
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const summArray = (arr) => {
-    return arr.reduce((res, val) => res += val, 0);
-}
+const summArray = (arr) => {return arr.reduce((res, val) => res += val, 0)};
 
 console.log('Сумма элементов массива: ', summArray(arr));
