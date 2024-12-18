5fe212cebf3346669f075274952ec29ae7d37bc6
Pavel <pavel@b2profit.ru>
2024-12-18 23:03:09 +0300

    1 лекция

--- Lect_1/script.js
+++ Lect_1/script.js
@@ -1,1 +1,9 @@
 // Запуск .js файла с с помощью Node.js
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const summArray = (arr) => {
+    return arr.reduce((res, val) => res += val, 0);
+}
+
+console.log('Сумма элементов массива: ', summArray(arr));
