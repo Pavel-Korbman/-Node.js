5f35729a64a7d2ee9d8b4f7bfeb327c5c250cf2a
Pavel <pavel@b2profit.ru>
2024-12-19 17:15:24 +0300

    1 ДЗ

--- DZ_1/server.js
+++ DZ_1/server.js
@@ -1,4 +1,9 @@
-// 
+// Напишите HTTP сервер и реализуйте два обработчика,где:
+// - По URL “/” будет возвращаться страница, на которой есть гиперссылка на вторую страницу по ссылке“/about”
+// - А по URL “/about” будет возвращаться страница, на которой есть гиперссылка на первую страницу“/”
+// - Также реализуйте обработку несуществующих роутов (404).
+// - * На каждой странице реализуйте счетчик просмотров. Значение счетчика должно увеличиваться на единицу каждый раз, когда загружается страница.
+
 const createCounter = () => {
     let counter = 0;
     return () => {
@@ -9,8 +14,8 @@ const counterHome = createCounter();
 const counterAbout = createCounter();
 
 const http = require('http');
-const server = http.createServer((reg, res) => {
-    console.log('Запрос получен');  
+const server = http.createServer((reg, res) => {  
+      
     if (reg.url === '/') {        
         res.writeHead(200, { 'content-type': 'text/html; charset=UTF-8' });
         res.end(`<h1>Корневая страница</h1><p>Просмотров: ${counterHome()}</p><a href="http://localhost:4000/about">Ссылка на страницу /about</a>`);
@@ -19,7 +24,7 @@ const server = http.createServer((reg, res) => {
         res.end(`<h1>Страница about</h1><p>Просмотров: ${counterAbout()}</p><a href="http://localhost:4000">Ссылка на страницу /</a>`);
     } else {
         res.writeHead(404, { 'content-type': 'text/html; charset=UTF-8' });
-        res.end('<h1>Страница не найдена</h1>');
+        res.end('<h1>404 Страница не найдена</h1>');
     }
 
 });
