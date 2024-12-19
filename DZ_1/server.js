a23a9920029e2ddb03df2c7e09a074c7fd58d463
Pavel <pavel@b2profit.ru>
2024-12-19 17:06:38 +0300

    1 ДЗ

--- DZ_1/server.js
+++ DZ_1/server.js
@@ -1,15 +1,22 @@
 // 
+const createCounter = () => {
+    let counter = 0;
+    return () => {
+        return ++counter;
+    }
+}
+const counterHome = createCounter();
+const counterAbout = createCounter();
 
 const http = require('http');
-
 const server = http.createServer((reg, res) => {
-    console.log('Запрос получен');
-    if (reg.url === '/') {
+    console.log('Запрос получен');  
+    if (reg.url === '/') {        
         res.writeHead(200, { 'content-type': 'text/html; charset=UTF-8' });
-        res.end('<h1>Привет. Добро пожаловать на мой сервер!</h1>');
-    } else if (reg.url === '/about') {
+        res.end(`<h1>Корневая страница</h1><p>Просмотров: ${counterHome()}</p><a href="http://localhost:4000/about">Ссылка на страницу /about</a>`);
+    } else if (reg.url === '/about') {       
         res.writeHead(200, { 'content-type': 'text/html; charset=UTF-8' });
-        res.end('<h1>Страница с информацией обо мне!</h1>');
+        res.end(`<h1>Страница about</h1><p>Просмотров: ${counterAbout()}</p><a href="http://localhost:4000">Ссылка на страницу /</a>`);
     } else {
         res.writeHead(404, { 'content-type': 'text/html; charset=UTF-8' });
         res.end('<h1>Страница не найдена</h1>');
