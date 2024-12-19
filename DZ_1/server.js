5823ab68c2a2c590ac21ab378be4bb1e3719306d
Pavel <pavel@b2profit.ru>
2024-12-19 17:18:18 +0300

    1 ДЗ

--- DZ_1/server.js
+++ DZ_1/server.js
@@ -14,23 +14,20 @@ const counterHome = createCounter();
 const counterAbout = createCounter();
 
 const http = require('http');
-const server = http.createServer((reg, res) => {  
-      
-    if (reg.url === '/') {        
+const server = http.createServer((reg, res) => {
+    if (reg.url === '/') {
         res.writeHead(200, { 'content-type': 'text/html; charset=UTF-8' });
         res.end(`<h1>Корневая страница</h1><p>Просмотров: ${counterHome()}</p><a href="http://localhost:4000/about">Ссылка на страницу /about</a>`);
-    } else if (reg.url === '/about') {       
+    } else if (reg.url === '/about') {
         res.writeHead(200, { 'content-type': 'text/html; charset=UTF-8' });
         res.end(`<h1>Страница about</h1><p>Просмотров: ${counterAbout()}</p><a href="http://localhost:4000">Ссылка на страницу /</a>`);
     } else {
         res.writeHead(404, { 'content-type': 'text/html; charset=UTF-8' });
         res.end('<h1>404 Страница не найдена</h1>');
     }
-
 });
 
 const port = 4000;
-
 server.listen(port, () => {
     console.log(`Сервер запущен на порту ${port}`);
 });
