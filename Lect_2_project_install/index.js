7ec08256909ae00e52c136f6650cd773f1ca1ee6
Pavel <pavel@b2profit.ru>
2024-12-22 20:20:16 +0300

    2 лекция установка своего пакета

---
+++ Lect_2_project_install/index.js
@@ -0,0 +1,5 @@
+const korbmanMath = require('korbman_project_1');
+const res1 = korbmanMath.summ(5, 7);
+const res2 = korbmanMath.dif(7,5);
+console.log(`5+7 =${res1}`);
+console.log(`7-5 =${res2}`);
