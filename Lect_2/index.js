44bd0ce66455379059a03e31e43595e8dcc57c92
Pavel <pavel@b2profit.ru>
2024-12-22 17:40:43 +0300

    2 лекция

---
+++ Lect_2/index.js
@@ -0,0 +1,9 @@
+// import { v4 as uuidv4 } from 'uuid';
+// uuidv4();
+
+// const { v4: uuidv4 } = require('uuid');
+// uuidv4();
+
+const uuid = require('uuid');
+const id = uuid.v4();
+console.log(id);
