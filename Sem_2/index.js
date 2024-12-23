611547e39cd0b5c6efd753a03f9c05142fba4ee0
Pavel <pavel@b2profit.ru>
2024-12-23 17:32:46 +0300

    2 семинар

--- Sem_2/index.js
+++ Sem_2/index.js
@@ -1,9 +1,12 @@
 // Задание №1 
 function calculateResultSumm(purchases, discount) {
     let total = purchases.reduce((acc, purchases) => acc += purchases, 0);
-    total = total*discount;
+    total = total * discount;
     return total;
 }
 
-const total = calculateResultSumm([12.1, 32.2, 43.1], 0.9);
-console.log(`Общая стоимость покупок: ${total} рублей.`);
+let Decimal = require('decimal.js-light');
+
+const total = new Decimal(calculateResultSumm([12.1, 32.2, 43.1], 0.9));
+
+console.log(`Общая стоимость покупок: ${total.toFixed(2)} рублей.`);
