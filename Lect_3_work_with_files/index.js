c1074d14ea492631c8af109920a4417ce394e895
Pavel <pavel@b2profit.ru>
2024-12-26 14:09:24 +0300

    3 лекция обзор встроенных модулей

--- Lect_3_work_with_files/index.js
+++ Lect_3_work_with_files/index.js
@@ -4,3 +4,21 @@
 
 // Модуль fs позволяет работать с файловой системой: 
 // читать и записывать файлы
+
+// Методfs.readFile() 
+// Для чтения файлов существует метод fs.readFile(). 
+// Он позволяет получить содержимое любого файла:
+
+const fs = require('fs');
+
+// fs.readFile(__filename, 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err)
+//     } else { console.log(data); } // текст этого файла
+// });
+
+fs.readFile('./hello.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err)
+    } else { console.log(data); }
+});
