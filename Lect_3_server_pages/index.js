f4bd2b23a0b04533bf0e9cd79bbbb07989f16cd0
Pavel <pavel@b2profit.ru>
2024-12-27 15:48:03 +0300

    3 лекция Возврат html файлов

--- Lect_3_server_pages/index.js
+++ Lect_3_server_pages/index.js
@@ -2,13 +2,35 @@
 
 // Писать HTML в JavaScript строке не очень удобно. Для удобства в express существует возможность отдавать в качестве ответа html файл с помощью метода res.sendFile()
 
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'static/index.html'));
+// });
+
+// const port = 5500;
+// app.listen(port, () => {
+//     console.log(`Сервер запущен на порту ${port}`);
+// });
+
+// Статичные файлы 
+// - это файлы, 
+// которые хранятся на сервере и доступны 
+// для загрузки и просмотра из браузера 
+
+// Для того, чтобы определить директорию хранения статичных файлов, необходимо использовать промежуточный обработчик express.static()
+
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
+app.use(express.static('static'));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static/index.html'));
+    res.sendFile('static/index.html');
 });
 
 const port = 5500;
@@ -16,3 +38,5 @@ app.listen(port, () => {
     console.log(`Сервер запущен на порту ${port}`);
 });
 
+// Теперь можно из браузера запустить все страницы напр. http://localhost:5500/about.html
+// Также можно разместить в static папке и подключить к html страницам css и js файлы 
