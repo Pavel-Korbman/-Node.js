72837370076ffb12821e9a1aa1534f229d8e687d
Pavel <pavel@b2profit.ru>
2024-12-30 02:08:55 +0300

    4 лекция

--- Lect_4_express_handlebars/index.js
+++ Lect_4_express_handlebars/index.js
@@ -1,29 +1,54 @@
 // Handlebars и Express
 
-const handlebars = require('handlebars');
+// const handlebars = require('handlebars');
+// const express = require('express');
+// const fs = require('fs');
+// const path = require('path');
+// const app = express();
+
+// const articles = [
+//     { title: 'Заголовок статьи 1', description: 'Текстовое наполнение первой статьи' },
+//     { title: 'Заголовок статьи 2', description: 'Текстовое наполнение второй статьи' },
+//     { title: 'Заголовок статьи 3', description: 'Текстовое наполнение третьей статьи' }    
+// ];
+
+// app.get('/', (req, res) => {
+//     const pathToTemplate = path.join(
+//         __dirname, '/templates/home.handlebars');
+
+//     fs.readFile(pathToTemplate, 'utf8', (err, data) => {
+//         if (err) {
+//             res.status(500);
+//             res.send(err.message);
+//         } else {
+//             const template = handlebars.compile(data);
+//             res.send(template({articles}));
+//         }
+//     });
+// });
+
+// app.listen(3000);
+
+// Библиотека express-handlebars
+// установка модуля: npm install express-handlebars
+
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    const pathToTemplate = path.joint(
-        __dirname, './templates/home.handlebars');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
-    fs.readFile(pathToTemplate, 'utf8', (err, data) => {
-        if (err) {
-            res.status(500);
-            res.send(err.message);
-        } else {
-            const template = handlebars.compile(data);
-            res.send(template({articles}));
-        }
-    });
+const articles = [
+    { title: 'Заголовок статьи 1', description: 'Текстовое наполнение первой статьи' },
+    { title: 'Заголовок статьи 2', description: 'Текстовое наполнение второй статьи' },
+    { title: 'Заголовок статьи 3', description: 'Текстовое наполнение третьей статьи' }    
+];
+
+app.get('/', (req, res) => {    
+res.render('home', {layout: 'index', title: 'Home', articles});
 });
 
-const port = 5000; // Задаём порт
-
-app.listen(port, () => {
-    console.log(`Сервер запущен на порту ${port}`);
-});
+app.listen(3000);
