e99a93e5c150bf71a8a85d9906a4107f34ea649d
Pavel <pavel@b2profit.ru>
2025-01-22 16:33:37 +0300

    4 лекция реализация API

--- Lect_4_API_Express/index.js
+++ Lect_4_API_Express/index.js
@@ -2,16 +2,62 @@
 // Рассмотрим реализацию API, в которой форматом передачи данных будет JSON. 
 // Это самый распространенный формат данных для межсервисного общения.
 
+// HTTP методы в Express:
+
+// const express = require('express');
+// const app = express();
+// app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.send('Привет!');
+// });
+// app.post('/', (req, res) => {
+//     res.send(req.body);
+// });
+// app.put('/', (req, res) => {
+//     res.send(req.body);
+// });
+// app.delete('/', (req, res) => {
+//     res.send(req.body);
+// });
+// app.listen(3000);
+
+
 // Метод POST
+
+// const express = require('express');
+// const app = express();
+// app.use(express.json());
+
+// app.post('/', (req, res)=>{
+// console.log(req.body);
+// res.send('<h1>Это POST запрос!</h1>');
+// });
+
+// app.listen(3000);
+
+// Метод PUT
+
+// const express = require('express');
+// const app = express();
+// app.use(express.json());
+
+// app.put('/', (req, res)=>{
+// console.log(req.body);
+// res.send('<h1>Это PUT запрос!</h1>');
+// });
+
+// app.listen(3000);
+
+// Метод DELETE
 
 const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.post('/', (req, res)=>{
+app.delete('/', (req, res)=>{
 console.log(req.body);
-res.send('<h1>Это POST запрос!</h1>');
+res.send('<h1>Это DELETE запрос!</h1>');
 });
 
 app.listen(3000);
-
