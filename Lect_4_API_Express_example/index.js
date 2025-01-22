d9cbccb4c27ad04b6c2b6ed88e479db29ba8a506
Pavel <pavel@b2profit.ru>
2025-01-22 22:54:50 +0300

    4 лекция пример API для работы с текстовыми блоками - статьями

--- Lect_4_API_Express_example/index.js
+++ Lect_4_API_Express_example/index.js
@@ -1,13 +1,216 @@
-// 💡 Реализация API в Express
-
-
+// 💡 Реализация API для управления статьями
+/*
 const express = require('express');
+const Joi = require('joi');
 const app = express();
 app.use(express.json());
 
-app.delete('/', (req, res)=>{
-console.log(req.body);
-res.send('<h1>Это DELETE запрос!</h1>');
+const articles = [];
+let uniqueID = 0;
+
+// Роут получения всех статей:
+app.get('/articles', (req, res) => {
+    res.send({ articles });
+});
+
+// Роут создания статьи:
+app.post('/articles', (req, res) => {
+    // Валидация запроса
+    if (!req.body.title) {
+        return res.status(400).send({error: 'Invalid title'});
+    }
+    if (!req.body.content) {
+        return res.status(400).send({error: 'Invalid content'});
+    }
+    if (req.body.content.length <=5) {
+        return res.status(400).send({error: 'Статья должна содержать больше 5 символов'});
+    }
+    
+    
+    uniqueID += 1;
+
+    articles.push({
+        id: uniqueID,
+        ...req.body
+    });
+
+    res.send({ id: uniqueID, });
+});
+
+// Роут получения статьи по параметру ID:
+app.get('/articles/:id', (req, res) => {
+    const article = articles.find((article) => article.id === Number(req.params.id));
+
+    if (article) {
+        res.send({ article });
+    } else {
+        res.status(404);
+        res.send({ article: null });
+    }
+});
+
+// Роут обновления статьи:
+app.put('/articles/:id', (req, res) => {
+    const article = articles.find((article) => article.id === Number(req.params.id));
+
+    if (article) {
+        article.title = req.body.title;
+        article.content = req.body.content;
+        res.send({ article });
+    } else {
+        res.status(404);
+        res.send({ article: null });
+    }
+});
+
+// Роут удаления статьи:
+app.delete('/articles/:id', (req, res) => {
+    const article = articles.find((article) => article.id === Number(req.params.id));
+
+    if (article) {
+        const articleIndex = articles.indexOf(article);
+        articles.splice(articleIndex, 1);
+        res.send({ article });
+    } else {
+        res.status(404);
+        res.send({ article: null });
+    }
+});
+
+app.listen(3000);
+*/
+
+// Валидатор Joi
+
+// const Joi = require('joi');
+
+// const schema = Joi.object({
+//     id: Joi.number().required(),
+//     title: Joi.string().min(1).required(),
+//     content: Joi.string().min(5).required()
+// });
+
+// const res = schema.validate({
+//     id: 1,
+//     title: 'Заголовок',
+//     content: '22'
+// });
+
+// console.log(res.error.details);
+// // [
+// //     {
+// //       message: '"content" length must be at least 5 characters long',
+// //       path: [ 'content' ],
+// //       type: 'string.min',
+// //       context: {
+// //         limit: 5,
+// //         value: '22',
+// //         encoding: undefined,
+// //         label: 'content',
+// //         key: 'content'
+// //       }
+// //     }
+// //   ]
+
+
+// Реализация API c валидацией Joi
+
+
+const express = require('express');
+const Joi = require('joi');
+const app = express();
+
+app.use(express.json());
+
+const idSchema = Joi.object({  // Схема валидации ID
+    id: Joi.number().required()
+});
+
+const articleSchema = Joi.object({  // Схема валидации статьи
+    title: Joi.string().required(),
+    content: Joi.string().min(5).required(),
+});
+
+const articles = []; // храним статьи в массиве (правильно считывать его из файла)
+let uniqueID = 0;
+
+app.get('/articles', (req, res) => {
+    res.send({ articles });
+});
+
+app.post('/articles', (req, res) => {
+    
+    const articleValidation = articleSchema.validate(req.body);
+    if(articleValidation.error) {
+        return res.status(400).send(articleValidation.error.details);
+    }    
+    
+    uniqueID += 1;
+
+    articles.push({
+        id: uniqueID,
+        ...req.body
+    });
+
+    res.send({ id: uniqueID, });
+});
+
+app.get('/articles/:id', (req, res) => {
+    const idValidation = idSchema.validate(req.params);
+    if(idValidation.error) {
+        return res.status(400).send(idValidation.error.details);
+    }
+
+    const article = articles.find((article) => article.id === Number(req.params.id));
+
+    if (article) {
+        res.send({ article });
+    } else {
+        res.status(404);
+        res.send({ article: null });
+    }
+});
+
+
+app.put('/articles/:id', (req, res) => {
+    const idValidation = idSchema.validate(req.params);
+    if(idValidation.error) {
+        return res.status(400).send(idValidation.error.details);
+    }
+
+    const articleValidation = articleSchema.validate(req.body);
+    if(articleValidation.error) {
+        return res.status(400).send(articleValidation.error.details);
+    }
+    
+    const article = articles.find((article) => article.id === Number(req.params.id));
+
+    if (article) {
+        article.title = req.body.title;
+        article.content = req.body.content;
+        res.send({ article });
+    } else {
+        res.status(404);
+        res.send({ article: null });
+    }
+});
+
+app.delete('/articles/:id', (req, res) => {
+    const idValidation = idSchema.validate(req.params);
+    if(idValidation.error) {
+        return res.status(400).send(idValidation.error.details);
+    }
+    
+    const article = articles.find((article) => article.id === Number(req.params.id));
+
+    if (article) {
+        const articleIndex = articles.indexOf(article);
+        articles.splice(articleIndex, 1);
+        res.send({ article });
+    } else {
+        res.status(404);
+        res.send({ article: null });
+    }
 });
 
 app.listen(3000);
